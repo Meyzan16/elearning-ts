eb1dbb45bc86f7e74469175956244eaccb877b85
Meyzan16 <meyzan1605@gmail.com>
2023-09-28 13:44:11 +0700

    create api test

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 import { registrationUser } from "../controllers/user.controller";
+import { testController } from "../controllers/test.controller";
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registrationUser);
+userRouter.get('/testt', testController);
 
 export default userRouter;
