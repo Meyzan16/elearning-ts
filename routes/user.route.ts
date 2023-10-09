cfacd63af33579b3bd8ae7f6391a2f603c84c9c8
Meyzan16 <meyzan1605@gmail.com>
2023-10-09 16:16:21 +0700

    create signin dan loogout

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
-import { activateUser, registrationUser } from "../controllers/user.controller";
+import { activateUser, loginUser, logoutUser, registrationUser } from "../controllers/user.controller";
 import { testController } from "../controllers/test.controller";
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registrationUser);
 userRouter.post('/activate-user', activateUser);
+userRouter.post('/login', loginUser);
+userRouter.get('/logout', logoutUser);
 
 export default userRouter;
