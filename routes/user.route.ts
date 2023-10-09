9c05ecfcc1eca188609aa45b104c5582ee4bdb61
Meyzan16 <meyzan1605@gmail.com>
2023-10-09 22:43:38 +0700

    create api logout and authenticate api logout

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -2,12 +2,13 @@ import express from "express";
 
 import { activateUser, loginUser, logoutUser, registrationUser } from "../controllers/user.controller";
 import { testController } from "../controllers/test.controller";
+import { isAuthenticated } from "../middleware/auth";
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registrationUser);
 userRouter.post('/activate-user', activateUser);
 userRouter.post('/login', loginUser);
-userRouter.get('/logout', logoutUser);
+userRouter.get('/logout', isAuthenticated, logoutUser);
 
 export default userRouter;
