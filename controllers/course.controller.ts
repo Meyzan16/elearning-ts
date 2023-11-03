af383e1c5caaea6390885395ba55bd5e142d68b5
Meyzan16 <meyzan1605@gmail.com>
2023-11-03 14:49:36 +0700

    create get single and all course

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -4,6 +4,7 @@ import ErrorHandler from "../utils/ErrorHandler";
 import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
 import CourseModel from "../models/course.model";
+import { redis } from "../utils/redis";
 
 //create course
 export const uploadCourse = CatchAsyncError(
@@ -27,34 +28,92 @@ export const uploadCourse = CatchAsyncError(
   }
 );
 
-export const editCourse = CatchAsyncError(async(req:Request, res:Response, next:NextFunction)=> {
-  try {
-    const data = req.body;
-    const thumbnail = data.thumbnail;
-    if(thumbnail){
-      await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+export const editCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
+      if (thumbnail) {
+        await cloudinary.v2.uploader.destroy(thumbnail.public_id);
 
-      const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
-        folder: "courses",
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+      const courseId = req.params.id;
+      const course = await CourseModel.findByIdAndUpdate(
+        courseId,
+        { $set: data },
+        { new: true }
+      );
+
+      res.status(201).json({
+        success: true,
+        course,
       });
-      data.thumbnail = {
-        public_id: myCloud.public_id,
-        url: myCloud.secure_url,
-      }; 
-    };
-    const courseId = req.params.id;
-    const course = await CourseModel.findByIdAndUpdate(courseId,
-      {$set: data},
-      {new: true});
-
-    res.status(201).json({
-      success:true,
-      course
-    });
-
-
-
-  } catch (error:any) {
-    return next(new ErrorHandler(error.message,400));
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
   }
-})
+);
+
+//get single course --- without purchasing
+export const getSingleCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courseId = req.params.id;
+      const isCacheExist = await redis.get(courseId);
+
+      if (isCacheExist) {
+        const course = JSON.parse(isCacheExist);
+        res.status(200).json({
+          status: true,
+          course,
+        });
+      } else {
+        const course = await CourseModel.findById(req.params.id).select(
+          "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+        );
+        await redis.set(courseId, JSON.stringify(course));
+        res.status(200).json({
+          success: true,
+          course,
+        });
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+//get all courses --- without purchasing
+export const getAllCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const isCacheExist = await redis.get("allCourses");
+      if (isCacheExist) {
+        const course = JSON.parse(isCacheExist);
+        res.status(200).json({
+          status: true,
+          course,
+        });
+      } else {
+        const course = await CourseModel.find().select(
+          "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+        );
+        await redis.set("allCourses", JSON.stringify(course));
+
+        res.status(200).json({
+          succes: true,
+          course,
+        });
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
